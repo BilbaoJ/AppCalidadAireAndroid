5b2f5cee954af21d311b6040d10e813273f2e470
BilbaoJ <jessicabilbao02@gmail.com>
2022-11-13 19:24:17 -0500

    Vista de la pantalla principal terminada

--- app/src/main/java/com/example/appcalidadaire/MainActivity.java
+++ app/src/main/java/com/example/appcalidadaire/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.appcalidadaire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton btnCalidadAire;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCalidadAire = findViewById(R.id.btnCalidadAire);
+
+        btnCalidadAire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityCalidadAire = new Intent(getApplicationContext(),
+                        CalidadAireActivity.class);
+                startActivity(activityCalidadAire);
+            }
+        });
     }
 }
